43c2dd38c2f4aae61d55e00ffc8a55c0cbe3b210
Prajwal P <prajwalp@Prajwals-MacBook-Air.local>
2024-08-21 16:43:11 +0530

    Allowed host

--- django_weather_app/settings.py
+++ django_weather_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-l)&hb#w1t_l$-+$gc95jkr(yu-+4w)g@v3&63#37jz(lq8%ja^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
